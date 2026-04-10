56b50ae6ffa6e07d5d021e3592e867e80f90b486
David C. S. <davidchamizosan@gmail.com>
2026-04-10 11:47:27 +0200

    Update package artifacts

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,13 @@
 """
 Quantum SMT Solver Core API.
-Expone las funciones de inicialización, oráculos y abstracciones lógicas.
 """
 
-# Importaciones relativas desde los submódulos internos
+# Imports abstractions about constructs crafted over gates acting on idividual qubits
 from .oracle_builder import oracle_less, oracle_eq, oracle_greatereq, oracle_interval
 from .abstractions import PhaseAND, PhaseNAND, PhaseOR, PhaseNOR
 from .grover_components import diffuser, ghz_init
 
-# Definición estricta de la API pública
+# Strict API definition 
 __all__ = [
     "oracle_less",
     "oracle_eq",
